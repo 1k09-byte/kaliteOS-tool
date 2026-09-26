b43f427376fac9cbded54533ab43d8ca9e2a155d
kucyveka <k3716509@gmail.com>
2026-09-25 17:42:00 -0700

    Adjust Proprietary License to permit usage but forbid code modification

--- src/kaliteConfig/Services/AppIconService.cs
+++ src/kaliteConfig/Services/AppIconService.cs
@@ -2,9 +2,10 @@
 // Copyright (c) 2026 kaliteConfig
 // All rights reserved.
 //
-// This software and associated documentation files are proprietary.
-// You may not use, copy, reproduce, modify, merge, publish, distribute, sublicense,
-// reverse-engineer, or sell copies of the software in any form, in whole or in part,
+// This software and associated documentation files are provided freely for end-users
+// to download and use. However, the source code remains strictly proprietary. 
+// You may not copy, reproduce, modify, merge, reverse-engineer, publish, distribute, 
+// sublicense, or sell copies of the source code in any form, in whole or in part,
 // without the express written permission of the copyright holder.
 // ==============================================================================
 using Microsoft.UI.Xaml.Media.Imaging;
